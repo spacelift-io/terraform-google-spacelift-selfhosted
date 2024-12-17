2886249a053d78d3b9ca92415b504d6c97497e87
peterdeme <snypox@gmail.com>
2024-12-17 16:23:38 +0100

    Remove trailing slash from Artifact domain

--- modules/artifacts/outputs.tf
+++ modules/artifacts/outputs.tf
@@ -1,5 +1,5 @@
 output "repository_domain" {
-  value       = "${google_artifact_registry_repository.spacelift.location}-docker.pkg.dev/"
+  value       = "${google_artifact_registry_repository.spacelift.location}-docker.pkg.dev"
   description = "The domain of the Docker repository"
 }
 
