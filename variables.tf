b19837dea3be73111e3829c830b364aa1218ab0f
Ievgen Khaliuzov <ievgenk@spacelift.io>
2026-05-22 16:25:50 +0100

    chore(drain): drop redundant default value note from variable descriptions

--- variables.tf
+++ variables.tf
@@ -149,49 +149,49 @@ variable "spacelift_version" {
 
 variable "drain_concurrency_async_jobs" {
   type        = number
-  description = "Number of concurrent receivers for the async-jobs queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the async-jobs queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_async_jobs_fifo" {
   type        = number
-  description = "Number of concurrent receivers for the async-jobs.fifo queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the async-jobs.fifo queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_cronjobs" {
   type        = number
-  description = "Number of concurrent receivers for the cronjobs queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the cronjobs queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_dlq" {
   type        = number
-  description = "Number of concurrent receivers for the DLQ queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the DLQ queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_dlq_fifo" {
   type        = number
-  description = "Number of concurrent receivers for the DLQ FIFO queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the DLQ FIFO queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_events" {
   type        = number
-  description = "Number of concurrent receivers for the events-inbox queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the events-inbox queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_iot" {
   type        = number
-  description = "Number of concurrent receivers for the IoT queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the IoT queue per drain pod."
   default     = 1
 }
 
 variable "drain_concurrency_webhooks" {
   type        = number
-  description = "Number of concurrent receivers for the webhooks queue per drain pod. Leave unset to keep the backend default of 1."
+  description = "Number of concurrent receivers for the webhooks queue per drain pod."
   default     = 1
 }
 
